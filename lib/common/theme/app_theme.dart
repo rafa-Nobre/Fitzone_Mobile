2c42e0d617a361a3ac3decbbc0691f08bad6f937
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-10-20 15:11:47 -0300

    style: add colors scheme and create dark and light themes

--- lib/common/theme/app_theme.dart
+++ lib/common/theme/app_theme.dart
@@ -1,31 +1,115 @@
 import 'package:flutter/material.dart';
 import 'colors.dart';
 
-ThemeData globalTheme(BuildContext context) {
-  return ThemeData(
-    primaryColor: primaryColor,
-    colorScheme: Theme.of(context).colorScheme.copyWith(secondary: secondaryColor),
-    appBarTheme: AppBarTheme(
-      elevation: 0.0,
-      iconTheme: const IconThemeData(
-        color: Colors.white,
-      ),
-      color: primaryColor,
-      titleTextStyle: const TextStyle(
-        color: Colors.white,
-        fontSize: 18,
-      ),
+ThemeData lightMode = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      brightness: Brightness.light,
+      background: backgroundLight,
+      surface: background2Light,
+      primary: themeColorLight,
+      secondary: themeColor2Light,
+      onPrimary: textLight,
+      onSecondary: textPlaceholderLight,
+      onBackground: textLight,
+      inversePrimary: textLight,
+      onInverseSurface: textDark,
     ),
-    scaffoldBackgroundColor: Colors.white,
-    splashColor: Colors.transparent,
-    hoverColor: Colors.transparent,
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: secondaryColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-      ),
+     textTheme: TextTheme(
+    headlineMedium: TextStyle(
+      color: textLight,
+      fontSize: 24,
     ),
-  );
-}
+    bodyLarge: TextStyle(
+      color: textLight,
+      fontSize: 16,
+    ),
+    bodyMedium: TextStyle(
+      color: textLight,
+      fontSize: 14,
+    ),
+    bodySmall: TextStyle(
+      color: textLight,
+      fontSize: 12,
+    ),
+  ),
+   appBarTheme: AppBarTheme(
+    backgroundColor: backgroundLight,
+    titleTextStyle: TextStyle(
+      color: textLight,
+      fontSize: 18,
+    ),
+  ),
+  
+);
+
+ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.light(
+    brightness: Brightness.dark,
+    background: backgroundDark,
+    surface: background2Dark,
+    primary: themeColorDark,
+    secondary: themeColor2Dark,
+    onPrimary: textDark,
+    onSecondary: textPlaceholderDark,
+    onBackground: textDark,
+    inversePrimary: textLight,
+    onInverseSurface: textLight,
+  ),
+  appBarTheme: AppBarTheme(
+    backgroundColor: backgroundDark,
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 18,
+    ),
+  ),
+  textTheme: TextTheme(
+    headlineMedium: TextStyle(
+      color: textDark,
+      fontSize: 24,
+    ),
+    bodyLarge: TextStyle(
+      color: textDark,
+      fontSize: 16,
+    ),
+    bodyMedium: TextStyle(
+      color: textDark,
+      fontSize: 14,
+    ),
+    bodySmall: TextStyle(
+      color: textDark,
+      fontSize: 12,
+    ),
+  ),
+);
+
+
+// ThemeData globalTheme(BuildContext context) {
+//   return ThemeData(
+//     primaryColor: themeColorLight,
+//     colorScheme: Theme.of(context).colorScheme.copyWith(secondary: secondaryColorLight),
+//     appBarTheme: AppBarTheme(
+//       elevation: 0.0,
+//       iconTheme: const IconThemeData(
+//         color: Colors.white,
+//       ),
+//       color: primaryColorLight,
+//       titleTextStyle: const TextStyle(
+//         color: Colors.white,
+//         fontSize: 18,
+//       ),
+//     ),
+//     scaffoldBackgroundColor: Colors.white,
+//     splashColor: Colors.transparent,
+//     hoverColor: Colors.transparent,
+//     elevatedButtonTheme: ElevatedButtonThemeData(
+//       style: ElevatedButton.styleFrom(
+//         backgroundColor: secondaryColorLight,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(8.0),
+//         ),
+//       ),
+//     ),
+//   );
+// }
--- lib/common/theme/colors.dart
+++ lib/common/theme/colors.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Colors.white;
-Color secondaryColor = Colors.white;
-Color cancelColor = Colors.white;
-Color confirmColor = Colors.white;
-Color alertColor = Colors.white;
+Color backgroundLight = const Color.fromRGBO(255, 255, 255, 1);
+Color textLight = const Color.fromRGBO(22, 22, 22, 1);
+Color textPlaceholderLight = const Color.fromRGBO(103, 118, 119, 1);
+Color themeColorLight = const Color.fromRGBO(209, 245, 97, 1);
+Color themeColor2Light = const Color.fromRGBO(162, 199, 46, 1);
+Color borderLight = const Color.fromRGBO(240, 240, 240, 1);
+Color constrastLight = const Color.fromRGBO(22, 22, 22, 1);
+Color background2Light = const Color.fromRGBO(249, 250, 251, 1);
+Color background3Light = const Color.fromRGBO(249, 250, 251, 1);
+
+Color backgroundDark = const Color.fromRGBO(25, 33, 38, 1);
+Color textDark = const Color.fromRGBO(251, 251, 251, 1);
+Color textPlaceholderDark = const Color.fromRGBO(169, 174, 180, 1);
+Color themeColorDark = const Color.fromRGBO(209, 245, 97, 1);
+Color themeColor2Dark = const Color.fromRGBO(209, 245, 97, 1);
+Color borderDark = const Color.fromRGBO(45, 54, 61, 1);
+Color constrastDark = const Color.fromRGBO(22, 22, 22, 1);
+Color background2Dark = const Color.fromRGBO(56, 64, 70, 1);
+Color background3Dark = const Color.fromRGBO(22, 22, 22, 1);
