28ea98b54e607c2215d11841c53208d905625fb4
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-10-20 15:13:04 -0300

    feat: add ThemeProvider class for change app themes

---
+++ lib/common/theme/theme_provider.dart
@@ -0,0 +1,23 @@
+import 'package:fitzone_app/common/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ThemeProvider with ChangeNotifier {
+
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  set themeData (ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme (){ 
+    _themeData = _themeData == lightMode 
+    ? darkMode 
+    : lightMode;
+      notifyListeners();
+  }
+  
+}
