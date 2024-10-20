fcb0bb010e785cc855983e2a95c27e2a43848f27
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-10-20 15:13:22 -0300

    feat: implement theme switching functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+
+import 'package:fitzone_app/common/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,11 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FitZone',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
@@ -42,27 +47,35 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: Text(widget.title, 
+        style: Theme.of(context).appBarTheme.titleTextStyle),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        color: Theme.of(context).colorScheme.background,        
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: (){
+          Provider.of<ThemeProvider>(context, listen:false).toggleTheme();
+        },
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        child: const Icon(Icons.add,),
       ),
     );
   }
