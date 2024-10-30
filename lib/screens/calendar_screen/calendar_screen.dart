1f51fb3030d7c56f02f1f2455d75fe1e20b2807d
Denys Oliveira <denys.nobre@outlook.com>
2024-10-30 10:34:45 -0300

    Finalizando mock primario da tela de calendario

--- lib/screens/calendar_screen/calendar_screen.dart
+++ lib/screens/calendar_screen/calendar_screen.dart
@@ -1,20 +1,37 @@
 import 'package:fitzone_app/common/constants/metrics.dart';
-import 'package:fitzone_app/common/theme/colors.dart';
 import 'package:fitzone_app/screens/calendar_screen/widgets/event_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:table_calendar/table_calendar.dart';
-
 import '../../common/widgets/NotificationButton.dart';
 import '../../common/widgets/Photo.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime todayDate = DateTime.now();
+  bool isListVisible = true;
+
+  void _onDaySelect(DateTime day, DateTime focusedDay) {
+    setState(() {
+      todayDate = day;
+      if (!isSameDay(day, DateTime.now())) {
+        isListVisible = false;
+      }else {
+        isListVisible = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("Atividades"),
         surfaceTintColor: Theme.of(context).colorScheme.primary,
         backgroundColor: Theme.of(context).colorScheme.primary,
         actions: const [
@@ -27,7 +44,7 @@ class CalendarScreen extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            color: Theme.of(context).colorScheme.inversePrimary,
+            color: Theme.of(context).colorScheme.surfaceContainer,
           ),
           Column(
             children: [
@@ -40,37 +57,61 @@ class CalendarScreen extends StatelessWidget {
                 ),
                 height: 380,
                 child: TableCalendar(
+                  headerStyle: const HeaderStyle(
+                      titleCentered: true, formatButtonVisible: false),
                   calendarStyle: CalendarStyle(
-                    //todayDecoration: BoxDecoration(shape:BoxShape.circle ,color: Theme.of(context).colorScheme.),
+                    todayDecoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Theme.of(context).colorScheme.surface),
+                    todayTextStyle: const TextStyle(color: Colors.black),
                     defaultTextStyle: const TextStyle(color: Colors.white),
                     defaultDecoration: BoxDecoration(
                         shape: BoxShape.circle,
                         color: Theme.of(context).colorScheme.inversePrimary),
                   ),
-                  focusedDay: DateTime.now(),
-                  currentDay: DateTime.now(),
+                  focusedDay: todayDate,
+                  selectedDayPredicate: (day) => isSameDay(day, todayDate),
                   firstDay: DateTime.utc(2024, 10, 1),
-                  lastDay: DateTime.utc(2024, 10, 29),
+                  lastDay: DateTime.utc(2024, 11, 29),
+                  onDaySelected: _onDaySelect,
                 ),
               ),
-              SingleChildScrollView(
+              const SizedBox(height: defaultSpacing),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: defaultSpacing),
+                    child: Text("Atividades de hoje",
+                        style: Theme.of(context).textTheme.bodyMedium),
+                  )),
+              const SizedBox(height: largeSpacing),
+              Visibility(
+                visible: isListVisible,
                 child: Padding(
                   padding: const EdgeInsets.all(defaultSpacing),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: minorSpacing),
-                        Text("Atividades de hoje", style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.white)),
-                        const SizedBox(height: defaultSpacing),
-                        EventCard(content: "Treino com Prof. Ramon Dino", hour: "7:00", isMarked: true, onMarkTask: (v){}),
-                        EventCard(content: "Aula de dança com Prof. Tainá", hour: "14:30", isMarked: false, onMarkTask: (v){}),
-                        EventCard(content: "Yoga com Prof. Gracyanne", hour: "19:00", isMarked: true, onMarkTask: (v){}),
-                      ],
-                    ),
+                  child: Column(
+                    children: [
+                      EventCard(
+                        content: "Treino com Prof. Ramon Dino",
+                        hour: "7:00",
+                        isMarked: true,
+                      ),
+                      const EventCard(
+                          content: "Aula de dança com Prof. Tainá",
+                          hour: "14:30",
+                          isMarked: false),
+                      const EventCard(
+                          content: "Yoga com Prof. Gracyanne",
+                          hour: "19:00",
+                          isMarked: true),
+                    ],
                   ),
                 ),
-              )
+              ),
+              Visibility(
+                visible: !isListVisible,
+                child: const Text("Nenhuma atividade disponível!"),
+              ),
             ],
           ),
         ],
--- lib/screens/calendar_screen/widgets/event_card.dart
+++ lib/screens/calendar_screen/widgets/event_card.dart
@@ -3,12 +3,15 @@ import 'package:fitzone_app/common/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 class EventCard extends StatefulWidget {
-  const EventCard({super.key, required this.content, required this.hour, this.isMarked, required this.onMarkTask});
+  const EventCard(
+      {super.key,
+      required this.content,
+      required this.hour,
+      this.isMarked});
 
   final String content;
   final String hour;
   final bool? isMarked;
-  final void Function(bool?) onMarkTask;
 
   @override
   State<EventCard> createState() => _EventCardState();
@@ -17,12 +20,12 @@ class EventCard extends StatefulWidget {
 class _EventCardState extends State<EventCard> {
   Color containerColor = Colors.grey;
 
-  Widget setIconState () {
-    if(widget.isMarked == null) {
+  Widget setIconState() {
+    if (widget.isMarked == null) {
       return const SizedBox();
-    }else if(widget.isMarked == true) {
+    } else if (widget.isMarked == true) {
       return const Icon(Icons.check);
-    }else if(widget.isMarked == false) {
+    } else if (widget.isMarked == false) {
       return const Icon(Icons.cancel);
     }
 
@@ -31,31 +34,53 @@ class _EventCardState extends State<EventCard> {
 
   @override
   void initState() {
-    if(widget.isMarked == null) {
+    if (widget.isMarked == null) {
       containerColor = Colors.grey;
-    }else if(widget.isMarked == true) {
+    } else if (widget.isMarked == true) {
       containerColor = themeColorLight;
-    }else {
+    } else {
       containerColor = Colors.red.shade300;
     }
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: minorSpacing),
-      child: Row(
-        children: [
-          Expanded(child: Text("${widget.content}\n${widget.hour}", style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Theme.of(context).colorScheme.background))),
-          //Radio(value: isMarked, groupValue: isMarked, onChanged: onMarkTask, )
-          Container(
-            height: 24,
-            decoration: BoxDecoration(shape: BoxShape.circle, color: containerColor),
-            child: setIconState(),
-          )
-        ],
+      child: GestureDetector(
+        onTap: () => showModalBottomSheet(
+          context: context,
+          builder: (context) => BottomSheet(
+            constraints: const BoxConstraints(minHeight: 100),
+            onClosing: () {},
+            builder: (context) => Padding(
+              padding: const EdgeInsets.all(minorSpacing),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Align(alignment: Alignment.center, child: Text("Informações do treino")),
+                  const SizedBox(height: defaultSpacing),
+                  Text("Detalhes: ${widget.content}"),
+                  Text("Hora: ${widget.hour}"),
+                ],
+              ),
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+                child: Text("${widget.content}\n${widget.hour}",
+                    style: Theme.of(context).textTheme.bodyMedium)),
+            Container(
+              height: 24,
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: containerColor),
+              child: setIconState(),
+            )
+          ],
+        ),
       ),
     );
   }
