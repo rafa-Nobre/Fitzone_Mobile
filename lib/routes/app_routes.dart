09e08c383de5fff7e21e4d1b7c3869365cb8301d
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-12-08 21:27:04 -0300

    feat: add route for today's workout screen

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:fitzone_app/screens/calendar_screen/calendar_screen.dart';
 import 'package:fitzone_app/screens/register_screen/register_screen.dart';
 import 'package:fitzone_app/screens/search_screen/search_screen.dart';
 import 'package:fitzone_app/screens/signin_screen/signin_screen.dart';
+import 'package:fitzone_app/screens/workout_screen/workout_page.dart';
 import 'package:flutter/widgets.dart';
 import 'routes_consts.dart';
 
@@ -11,4 +12,5 @@ final Map<String, Widget Function(BuildContext)> appRoutes = {
   RoutesConsts.home: (context) => const SizedBox(),
   RoutesConsts.calendar: (context) => const CalendarScreen(),
   RoutesConsts.search: (context) => const SearchScreen(),
+   RoutesConsts.todaysWorkout: (context) => const WorkoutPage(),
 };
